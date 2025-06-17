fdd1e4dbf1b59fabc1a0ef8c8c5e09346bb0c584
P-WJ <pwj1103@gmail.com>
2025-06-17 15:12:48 +0900

    Spring Boot jdbc 250617

---
+++ work_backend/spring_jdbc_250617/src/main/java/com/hd/spring_jdbc_250617/SpringJdbc250617Application.java
@@ -0,0 +1,13 @@
+package com.hd.spring_jdbc_250617;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbc250617Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbc250617Application.class, args);
+	}
+
+}
