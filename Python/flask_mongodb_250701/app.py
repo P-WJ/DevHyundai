68423c1e5454fa33ecdd6273025559dcf3708895
P-WJ <pwj1103@gmail.com>
2025-07-02 14:30:19 +0900

    flask_mongodb 250702

--- Python/flask_mongodb_250701/app.py
+++ Python/flask_mongodb_250701/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
-
+from flask_cors import CORS
+from apscheduler.schedulers.background import BackgroundScheduler
+from scheduler.job import fetch_and_store
+from routes.boxoffice import get_box_office
 from routes.product import create_product, get_all_products, root_path, update_product, delete_product
 
 app = Flask(__name__)
@@ -10,6 +13,12 @@ app.add_url_rule("/products", view_func=create_product, methods=['POST'])
 app.add_url_rule("/products", view_func=get_all_products, methods=['GET'])
 app.add_url_rule("/products/<name>", view_func=update_product, methods=['PUT'])
 app.add_url_rule("/products/<name>", view_func=delete_product, methods=['DELETE'])
+app.add_url_rule("/api/boxoffice", view_func=get_box_office, methods=['GET'])
+
+# 스케줄러 등록
+scheduler = BackgroundScheduler()
+scheduler.add_job(fetch_and_store, 'cron', hour=12, minute=37)
+scheduler.start()
 
 if __name__ == "__main__":
     app.run()
---
+++ Python/flask_mongodb_250701/scheduler/job.py
@@ -0,0 +1,23 @@
+import requests  # HTTP 요청과 응답
+from datetime import datetime, timedelta  # 운영체제로 부터 시간 정보를 가져옴
+from db.mongodb import collection  # DB Table 정보를 가져옴
+
+API_KEY = "88e8af5c07ec04486d77c628b60b1154"
+
+
+def fetch_and_store():  # 스케줄러에 의해서 정해진 시간에 호출
+    print("fetch_and_store 호출 됨!!!!")
+    yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d")
+    url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
+    params = {"key": API_KEY, "targetDt": yesterday}
+
+    res = requests.get(url, params=params)
+    if res.status_code == 200:
+        result = res.json()
+        data = result["boxOfficeResult"]["dailyBoxOfficeList"]
+
+        # 오늘 날짜 기준 데이터 덮어쓰기
+        collection.delete_many({"targetDt": yesterday})
+        for item in data:
+            item["targetDt"] = yesterday
+            collection.insert_one(item)
